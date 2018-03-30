a87332662e8d21b8b44f680401622acc605d1669
Kitty Davies <kittyfdavies@gmail.com>
2018-03-30 01:19:43 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -13,7 +13,7 @@ class CashRegister
   def add_item(title, price, quantity = 1)
     @array = []
     @total += price * quantity
-    if quantity
+    if quantity > 1
       counter = 0
       until counter == quantity
         @array << title
